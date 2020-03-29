dadc24fa0b12c80d147e6c3ebe4418124e947a08
theniikhil <imn.parashar@gmail.com>
2020-03-29 16:49:36 +0530

    static burger

---
+++ burger-builder/src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Layout.module.css'
+const layout = (props) => (
+    <React.Fragment>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className = {classes.Content}>
+            {props.children}
+        </main>
+    </React.Fragment>
+)
+
+export default layout;
